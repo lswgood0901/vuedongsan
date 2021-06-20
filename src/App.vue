d5763f2fa4790119cad6fe031c5b8cfba764ed47
lswgood0901 <83624931+lswgood0901@users.noreply.github.com>
2021-06-21 00:45:25 +0900

    test1

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,32 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+<div id="app">
+    <div class ="menu">
+      <a v-for="(menuname, i) in menus" :key="i">{{menuname}}</a>
+
+    </div>
+
+
+
+    <div v-for="(item, i) in products" :key="i">
+      <h4>{{item[0]}}</h4>
+      <p>{{item[1]}}</p>
+    </div>
+
+ </div>
+
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
 
 export default {
   name: 'App',
+  data(){
+    return {
+      menus : ['Home', 'Shop', 'About'],
+      products : [['역삼동원룸', 80 +' 만원'], ['천호동원룸', 60+' 만원'], ['마포구원룸', 90 +' 만원']],
+    }
+  },
   components: {
-    HelloWorld
   }
 }
 </script>
@@ -23,6 +38,14 @@ export default {
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
+}
+.menu {
+  background: darkslateblue;
+  padding: 15px;
+  border-radius: 5px;
+}
+.menu a {
+  color: white;
+  padding: 10px;
 }
 </style>
